ed1c10868f060eb944f8cfc4e0313f22e17b9f90
Emlulu24 <145481428+Emlulu24@users.noreply.github.com>
2024-01-12 15:32:58 +0000

    colors

--- public/main.js
+++ public/main.js
@@ -1,4 +1,9 @@
 // Write JavaScript here.
 console.log('main.js loaded successfully!')
 
-letStyleElement = document.querySelector("#info");
+for (let li of document.querySelectorAll("#colors")) {
+  li.addEventListener("click", function (event) {
+    event.target.style.backgroundColor = "grey";
+    event.target.style.color = "blue";
+  });
+}
